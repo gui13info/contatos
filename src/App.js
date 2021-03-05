ace43fd8442b8d17b6d41112bb909608ab6111dc
Guilherme Matos <marques.guilherme0067@gmail.com>
2021-03-04 21:03:24 -0300

    Aula #2 - Entendendo componentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ListaContatos from './components/ListaContatos';
+
 function App() {
   return (
     <div>
       <h1>Contatos</h1>
+      <ListaContatos />
     </div>
   );
 }
---
+++ src/components/ListaContatos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ListaContatos(){
+    return(
+        <div>
+            <h1>Lista</h1>
+        </div>
+    );
+}
