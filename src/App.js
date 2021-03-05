c4f2b779ef8f734690606e7b306bf675eb35c6ee
Guilherme Matos <marques.guilherme0067@gmail.com>
2021-03-05 18:44:45 -0300

    Aula #3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import ListaContatos from './components/ListaContatos';
+import './global.css';
 
 function App() {
+  let state= {
+    contatos: [
+      {
+        "id": "0",
+        "nome": "Guilherme",
+        "instagram": "@guimarquess13"
+      },
+      {
+        "id": "1",
+        "nome": "Leonardo",
+        "instagram": "@leonardo13"
+      },
+      {
+        "id": "2",
+        "nome": "Maria",
+        "instagram": "@mari13"
+      },
+    ]
+  }
   return (
     <div>
       <h1>Contatos</h1>
-      <ListaContatos />
+      <ListaContatos contatos={state.contatos}/>
     </div>
   );
 }
--- src/components/ListaContatos.js
+++ src/components/ListaContatos.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export default function ListaContatos(){
+export default function ListaContatos(props){
     return(
         <div>
             <h1>Lista</h1>
+            <ul style={{listStyleType: 'nome', padding: '0'}}>
+                {props.contatos.map(contato => (
+                    <li key={contato.id}>
+                        <h4>{contato.nome}</h4>
+                        <p>{contato.instagram}</p>
+                        <button>Excluir</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
